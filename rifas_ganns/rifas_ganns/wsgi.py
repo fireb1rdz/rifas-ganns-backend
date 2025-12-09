6e2f2661203f5f920a102ee998532d6ba7051445
fireb1rdz <gustavonelsonpaganelli@gmail.com>
2025-12-09 20:07:32 -0300

    updating wsgi

--- rifas_ganns/rifas_ganns/wsgi.py
+++ rifas_ganns/rifas_ganns/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rifas_ganns.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rifas_ganns.rifas_ganns.settings')
 
 application = get_wsgi_application()
